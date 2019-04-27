091b2b11d126dc6d60aa3b4a61d31ed0ce0ed6b7
Ethan <ethan.harriz@gmail.com>
2019-04-27 17:02:48 -0400

    We moved

--- django_logs/home.py
+++ django_logs/home.py
@@ -33,7 +33,7 @@ embed = json.dumps({
         },
         {
             "name": "How do I use this?",
-            "value": "Simply go to https://v.ej.gl/view?url=yoururlhere",
+            "value": "Simply go to https://lol-sa.me/view?url=yoururlhere",
             "inline": True
         },
         {
