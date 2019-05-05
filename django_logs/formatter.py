a84f063466b2476a877988f6f90816578ff81ae0
Ethan <ethan.harriz@gmail.com>
2019-05-04 22:34:38 -0400

    Added back Discord's emoji labels with fallback, added proper img alt handling

--- django_logs/formatter.py
+++ django_logs/formatter.py
@@ -70,11 +70,12 @@ def format_content_html(content: str, masked_links: bool = False, newlines: bool
     def process_unicode_emojis(m, text):
         e = m.group()
         e = re.sub(r'[\U0000FE00-\U0000FE0F]$', '', e)
-        title = UNICODE_LIST.get(e) or demoji._CODE_TO_DESC[e]
+        title_e = re.sub(r'[\U0001F3FB-\U0001F3FF]$', '', e)
+        title = UNICODE_LIST.get(title_e) or demoji._CODE_TO_DESC[title_e]
         emoji_class = 'emoji emoji--large' if is_jumboable(demoji._EMOJI_PAT, text) else 'emoji'
         codepoint = "-".join(['%04x' % ord(_c) for _c in e]).lstrip('0')
-        return fr'<img class="{emoji_class}" title="{title}" ' \
-            fr'src="https://twemoji.maxcdn.com/2/svg/{codepoint}.svg" alt=":{title}:">'
+        return fr'<img class="{emoji_class}" title=":{title}:" ' \
+            fr'src="https://twemoji.maxcdn.com/2/svg/{codepoint}.svg" alt="{e}">'
 
     # Process unicode emojis
     content = demoji.replace(content, lambda m: process_unicode_emojis(m, content))
@@ -236,10 +237,11 @@ def format_micro_content_html(content: str, newlines: bool = True) -> str:
     def process_unicode_emojis(m):
         e = m.group()
         e = re.sub(r'[\U0000FE00-\U0000FE0F]$', '', e)
-        title = UNICODE_LIST.get(e) or demoji._CODE_TO_DESC[e]
+        title_e = re.sub(r'[\U0001F3FB-\U0001F3FF]$', '', e)
+        title = UNICODE_LIST.get(title_e) or demoji._CODE_TO_DESC[title_e]
         codepoint = "-".join(['%04x' % ord(_c) for _c in e]).lstrip('0')
-        return fr'<img class="emoji" title="{title}" ' \
-            fr'src="https://twemoji.maxcdn.com/2/svg/{codepoint}.svg" alt=":{title}:">'
+        return fr'<img class="emoji" title=":{title}:" ' \
+            fr'src="https://twemoji.maxcdn.com/2/svg/{codepoint}.svg" alt="{e}">'
 
     # Process unicode emojis
     content = demoji.replace(content, process_unicode_emojis)
