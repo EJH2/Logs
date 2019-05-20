8484ffe5f92683343886b8a51e5151c1adb5d97b
Ethan <ethan.harriz@gmail.com>
2019-05-19 23:08:17 -0400

    Make redis url configurable

--- django_logs/celery.py
+++ django_logs/celery.py
@@ -1,12 +1,16 @@
 import os
 from celery import Celery
 
+from decouple import config
+
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_logs.settings')
 
+redis_url = f'redis://{config("REDIS_PASSWORD")}@{config("REDIS_HOST")}:{config("REDIS_PORT")}/{config("REDIS_DB")}'
+
 app = Celery('django_logs',
-             broker='redis://',
-             backend='redis://',
+             broker=redis_url,
+             backend=redis_url,
              include=['django_logs.tasks'])
 
 # Using a string here means the worker doesn't have to serialize
