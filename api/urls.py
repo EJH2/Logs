67a9e7ee47168d7161226f557b38ce8560270503
Ethan <ethan.harriz@gmail.com>
2019-05-30 19:57:56 -0400

    URL regexes are stupid

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', get_swagger_view(title='Django Logs API')),
     path('logs/', views.LogView.as_view()),
     path('logs/create/', views.LogCreate.as_view()),
-    re_path(r'logs/delete/(?P<short_code>[\w]{5})', views.LogDestroy.as_view()),
-    re_path(r'logs/(?P<short_code>[\w]{5})', views.LogRead.as_view()),
+    re_path(r'logs/delete/(?P<short_code>[\w]{5})?', views.LogDestroy.as_view()),
+    re_path(r'logs/(?P<short_code>[\w]{5})?', views.LogRead.as_view()),
     re_path(r'traceback/', views.traceback, name='traceback'),
 ]
