7ad63280a7c67f03df6f4b42e98ab4d6bc14a684
Ethan <ethan.harriz@gmail.com>
2019-12-24 16:41:24 -0500

    Bump package version, remove pinned package numbers because it causes problems on fresh installs, will revisit later

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='discord_logview',
-    version='2.0.1',
+    version='2.0.2',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
@@ -17,7 +17,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
@@ -27,27 +27,27 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires=[
-        "natural==0.2.0",
-        "celery==4.3.0",
-        "celery-progress==0.0.8",
-        "channels==2.3.0",
-        "channels-redis==2.4.0",
-        "demoji==0.1.5",
-        "django==2.2.8",
-        "django-allauth==0.40.0",
-        "djangorestframework==3.10.2",
-        "drf-yasg==1.16.1",
-        "itsdangerous==1.1.0",
-        "pendulum==2.0.5",
-        "psycopg2==2.8.3",
-        "python-decouple==3.1",
-        "redis==3.3.8",
-        "requests==2.22.0",
-        "sentry-sdk==0.12.3",
-        "shortuuid==0.5.0"
+        "natural",
+        "celery",
+        "celery-progress",
+        "channels",
+        "channels-redis",
+        "demoji",
+        "django",
+        "django-allauth",
+        "djangorestframework",
+        "drf-yasg",
+        "itsdangerous",
+        "pendulum",
+        "psycopg2",
+        "python-decouple",
+        "redis",
+        "requests",
+        "sentry-sdk",
+        "shortuuid"
     ],
     extras_require={
-        'win': ["gevent==1.4.0"],
-        'test': ["responses==0.10.9"]
+        'win': ["gevent"],
+        'test': ["responses"]
     }
 )
