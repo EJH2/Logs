0d84de2d5789cd54f8661303d4fb94cc82b14276
Ethan <ethan.harriz@gmail.com>
2020-08-27 21:49:00 -0400

    Remove unnecessary variables from error handlers

--- web/views.py
+++ web/views.py
@@ -238,15 +238,15 @@ def log_preview_export(request, pk):
 #            Error Handlers
 # ====================================
 
-def handle400(request, exception):
+def handle400(request, *args, **kwargs):
     return render(request, '400.html', status=400)
 
 
-def handle403(request, exception):
+def handle403(request, *args, **kwargs):
     return render(request, '403.html', status=403)
 
 
-def handle404(request, exception):
+def handle404(request, *args, **kwargs):
     return render(request, '404.html', status=404)
 
 
