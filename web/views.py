b084720a9e4a921c04b2d1e7d4f9ec4f94a4d806
Ethan <ethan.harriz@gmail.com>
2020-12-05 18:03:03 -0500

    Remove unused import

--- web/views.py
+++ web/views.py
@@ -4,7 +4,6 @@ from allauth.socialaccount.models import SocialAccount
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
-from django.core.exceptions import SuspiciousOperation
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404, redirect
 from itsdangerous import BadSignature
