34e1ea5f70eb5ae6750a7ee806ecfc64fd3c87fe
Ethan <ethan.harriz@gmail.com>
2020-12-17 02:13:16 -0500

    Add new permissions method filter_view_queryset to show logs that users can see, previously web's _get_privacy

--- api/permissions.py
+++ api/permissions.py
@@ -23,3 +23,22 @@ def filter_queryset(request, queryset):
         ])
     except SocialAccount.DoesNotExist:
         return queryset.filter(owner=request.user)
+
+
+def filter_view_queryset(request, queryset):
+    if not request.user.is_authenticated:
+        return queryset.none()
+    if request.user.is_staff:
+        return queryset.all()
+    try:
+        discord_user = SocialAccount.objects.get(user=request.user)
+        _guilds = discord_user.extra_data.get('guilds', [])
+        guilds = [int(g['id']) for g in _guilds]
+        mod_guilds = [
+            # Check if user is owner of guild or has manage messages perm
+            int(g['id']) for g in _guilds if g['owner'] or (g['permissions'] >> 13) & 1
+        ]
+        return queryset.filter(owner=request.user) | queryset.filter(guild__in=guilds, privacy='guild') | \
+            queryset.filter(guilds__in=mod_guilds, privacy='mods')
+    except SocialAccount.DoesNotExist:
+        return queryset.filter(owner=request.user)
