000968515548d7689eb41819a439104be2f9b243
Ethan <ethan.harriz@gmail.com>
2023-04-20 21:46:17 -0400

    Fix 500 error when responding to requests with no expiry

--- api/utils.py
+++ api/utils.py
@@ -7,13 +7,13 @@ signer = URLSafeSerializer(settings.SECRET_KEY)
 
 
 def validate_expires(user, value):
-    value = pendulum.instance(value)
+    value = pendulum.instance(value) if value else None
     exp = {'weeks': 1}
     if user.has_perm('api.extended_expiry'):
         exp = {'months': 1}
     if user.has_perm('api.no_expiry'):
         exp = None
-    if exp and value > pendulum.now().add(**exp):
+    if exp and (value or pendulum.datetime(year=9999, month=12, day=31)) > pendulum.now().add(**exp):
         raise serializers.ValidationError(
             f'Expiry time must be an iso-8601 timestamp of a date less than '
             f'{", ".join(["{} {}".format(exp[k], k[:-1 if k.endswith("s") else k]) for k in exp])} from now!'
